9e436e3b3a96429442925842731d92a1449799e3
adriangm95 <115050376+adriangm95@users.noreply.github.com>
2022-12-15 00:52:39 +1200

    Add files via upload

---
+++ insert_hspedaje.sql
@@ -0,0 +1,3 @@
+INSERT INTO evfinaldb.hospedaje VALUES (1,'Sol de Paracas');
+INSERT INTO evfinaldb.hospedaje VALUES (2,'Caminos del Inka');
+INSERT INTO evfinaldb.hospedaje VALUES (3,'Paracas Lounge');
